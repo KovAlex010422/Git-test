71ac3d97a6fc62ca68d46a12c6119eae5c8827b8
KovAlex01.04.22 <koval4ykoo1@gmail.com>
2022-09-12 20:28:43 +0300

    I'm starting doing task from Metaeducation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.body.innerHTML = '<h1> Hello from JS </h1>'
+
+const items = [
+    { price: 40},
+    { price : 120},
+    { price: 505},
+    { price: 350}];
+  
+  for (const item of items){
+  console.log(`Price: ${item.price}`);
+  }
