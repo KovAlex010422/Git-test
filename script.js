fe13c43444b93fad59d5542ee89708f16e203ad7
KovAlex01.04.22 <koval4ykoo1@gmail.com>
2022-10-23 21:18:57 +0300

    exercise 41a

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
 'use strict';
+const restorantData = {
+  menu: [
+      {
+          name: 'Salad Caesar',
+          price: '14$'
+      },
+      {
+          name: 'Pizza Diavola',
+          price: '9$'
+      },
+      {
+          name: 'Beefsteak',
+          price: '17$'
+      },
+      {
+          name: 'Napoleon',
+          price: '7$'
+      }
+  ],
+  waitors: [
+      {name: 'Alice', age: 22}, {name: 'John', age: 24}
+  ],
+  averageLunchPrice: '20$',
+  openNow: true
+};
 
+function isOpen(prop) {
+  let answer;
+  !prop ? answer = 'Закрыто' : answer = 'Открыто';
+  return answer;
+}
+
+console.log(isOpen(restorantData.openNow))
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+  if ((+fDish.price.slice(0, -1) + +sDish.price.slice(0, -1)) < +average.slice(0, -1)) {
+      return 'Цена ниже средней';
+  } else {
+      return 'Цена выше средней';
+  }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+  const copy = Object.assign({}, data);
+  copy.waitors = [{name: 'Mike', age: 32}];
+  return copy
+}
+console.log(restorantData);
+console.log(transferWaitors(restorantData));
+
+/*3) Функция transferWaitors создана для того, чтобы копировать шаблон данных и передавать их в другой ресторан. Конечно, в другом ресторане будут другие блюда, другие официанты и тп. Сейчас эта функция только в начале разработки и должна менять данные про официантов.
+
+Но в нынешнем виде мы обнаружили, что после её запуска не только копия данных содержит новых официантов, но и основные данные! В restorantData сотрудник Alice исчезает и заменяется Mike! Необходимо найти причину и немедленно исправить, чтобы данные были разделены.
+
+/* 
 const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
 function sortStudentsByGroups(arr) {
