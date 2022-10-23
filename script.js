16c3369b39abf93694e2d521516f9ba7c42c56a8
KovAllaptop <koval4ykoo1@gmail.com>
2022-10-23 11:43:46 +0300

    exersice about group students

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 'use strict';
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    let sorted = students.sort();
+    let result = [];
+    for(let i = 0; i < sorted.length; i+=3){
+      console.log(result[i])
+
+        result.push(sorted.slice(i, i+3))
+    }
+        for(let k = 0; k < result.length; k++){
+      if(result[k].length == 0){
+        result[k] = 'Оставшиеся студенты: -'
+      }
+      else if(result[k].length <3){
+        result[k] = 'Оставшиеся студенты: ' + result[k].join(', ')
+      }
+    }
+    return result
+}
+console.log(sortStudentsByGroups(students))
+/* Shops heating -------------------------------
 const shoppingMallData = {
   shops: [
       {
